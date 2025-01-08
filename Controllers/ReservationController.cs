c17770c6aeb4bb9a916cda30d65a0b32d4456f81
Igor Imprić <iimpric@gmail.com>
2025-01-08 11:42:36 +0100

    models-setup-cleanStart

---
+++ Controllers/ReservationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrizerskiSalon1.Controllers
+{
+    public class ReservationController : Controller
+    {
+        // Akcija za prikaz forme za kreiranje rezervacije
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
